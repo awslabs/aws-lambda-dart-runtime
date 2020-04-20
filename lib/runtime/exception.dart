4f0bf2b56275807dc4768ce5dbc368bf198ffe4a
Sebastian Döll <sebastian@katallaxie.me>
2020-04-20 17:49:20 +0200

    (fix) adding exception test

--- lib/runtime/exception.dart
+++ lib/runtime/exception.dart
@@ -1,6 +1,11 @@
+/// An exception thrown when there is an error in the runtime.
 class RuntimeException implements Exception {
-  String cause;
-  RuntimeException(this.cause);
+  const RuntimeException(this.cause);
 
+  /// Explains the reason why there is an exception thrown
+  /// in the runtime.
+  final String cause;
+
+  @override
   String toString() => "RuntimeException: $cause";
 }
